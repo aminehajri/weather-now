751ed6d15bb834206842762a7e38a7004c4d3a8d
Hajri01 <mohamedamine.hajri@gmail.com>
2020-09-25 15:59:28 +0100

    Initial commit

---
+++ app/src/main/java/com/hajri/weathernow/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hajri.weathernow.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hajri.weathernow.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
